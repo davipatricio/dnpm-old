ab2a061e9fbe94c93aef5d2de6d181e6b2a33868
Davi Patricio <davipatricio@pm.me>
2022-06-21 18:51:13 -0300

    feat: use gookit/color instead of davipatricio/colors (#6)
    use gookit/color instead of davipatricio/colors

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module dnpm
 
 go 1.18
 
-require github.com/davipatricio/colors v1.0.0
+require (
+	github.com/gookit/color v1.5.1 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
+)
--- messages/emptyCmd.go
+++ messages/emptyCmd.go
@@ -4,8 +4,7 @@ import (
 	"dnpm/utils"
 	"fmt"
 
-	"github.com/davipatricio/colors/colors"
-	"github.com/davipatricio/colors/styles"
+	"github.com/gookit/color"
 )
 
 func EmptyCmd(notSpecified bool) {
@@ -18,22 +17,22 @@ func EmptyCmd(notSpecified bool) {
 
 func emptyCmdRaw(notSpecified bool) {
 	if notSpecified {
-		fmt.Println(colors.Red("\nYou must specify a command."))
+		color.Red.Println("\nYou must specify a command.")
 	}
 	fmt.Printf("dnpm <command>\n\nUsage:\n\n")
 
-	fmt.Println(styles.Bold("dnpm install"), "         install all the dependecies in your project")
-	fmt.Println(styles.Bold("dnpm install <pkg>"), "   add the <pkg> dependecy to your project")
-	fmt.Println(styles.Bold("dnpm version"), "         shows the version of dnpm")
+	fmt.Println(color.OpBold.Render("dnpm install"), "         install all the dependecies in your project")
+	fmt.Println(color.OpBold.Render("dnpm install <pkg>"), "   add the <pkg> dependecy to your project")
+	fmt.Println(color.OpBold.Render("dnpm version"), "         shows the version of dnpm")
 }
 
 func emptyCmdEmojis(notSpecified bool) {
 	if notSpecified {
-		fmt.Println(colors.Red("\n❌ You must specify a command."))
+		color.Red.Println("\n❌ You must specify a command.")
 	}
 	fmt.Printf("dnpm <command>\n\nUsage:\n\n")
 
-	fmt.Println(colors.Green("➕"), styles.Bold("dnpm install"), "         install all the dependecies in your project")
-	fmt.Println(colors.Green("➕"), styles.Bold("dnpm install <pkg>"), "   add the <pkg> dependecy to your project")
-	fmt.Println(colors.Cyan(" ℹ️"), styles.Bold("dnpm version"), "         shows the version of dnpm")
+	fmt.Println("➕ ", color.OpBold.Render("dnpm install"), "         install all the dependecies in your project")
+	fmt.Println("➕ ", color.OpBold.Render("dnpm install <pkg>"), "   add the <pkg> dependecy to your project")
+	fmt.Println(color.Cyan.Render(" ℹ️"), color.OpBold.Render("dnpm version"), "         shows the version of dnpm")
 }
